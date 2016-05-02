2355e2cd1eb15e5728a0b24b2d4d94174188e4e7
Zeeshan Abid <zeesh.rocky@gmail.com>
2016-05-03 01:01:58 +0530

    ACD_JAVAB_Session_1_Assignment1
    Addition of two Numbers

---
+++ Assignment1/src/Addition/add.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Addition;
+
+/**
+ * @author zeesh
+ *
+ */
+public class add {
+
+	/**
+	 * @param args
+	 */
+	private static void sum(int a, float b) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		float b=30;
+		
+		int c=(int) (a+b);
+		
+		System.out.println("Sum of two numbers are: "+c);
+
+	}
+
+
+
+}
